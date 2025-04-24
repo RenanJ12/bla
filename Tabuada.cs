d951a35de6512385bef1f853a601b5bd20dda673
Renan Jesus De Oliveira <142060827+RenanJ12@users.noreply.github.com>
2025-04-23 22:22:28 -0300

    Tabuada

---
+++ Tabuada.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Aplicações_Matematicas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tabPage1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            txtnumtab.Clear();
+            lsttab.Items.Clear();
+            txtnumtab.Focus();
+        }
+
+        private void btncaltab_Click(object sender, EventArgs e)
+        {
+
+            int num, c;
+            try { 
+            num = Convert.ToInt32(txtnumtab.Text);
+            lsttab.Items.Clear();
+            for (c = 1; c <= 10; c++)
+            {
+                lsttab.Items.Add(num + " x " + c + " = " + (num * c));
+            }
+            }   
+            catch
+            {
+                MessageBox.Show("Favor,Coloque um número valido");
+            }
+        }
+    }
+}
